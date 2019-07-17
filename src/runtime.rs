68936466d63a8eae6b55eca18efe8b2635ae5ae0
cohyou <cohyou@aurola.net>
2019-07-17 21:22:14 +0900

    can not build...

--- src/runtime.rs
+++ src/runtime.rs
@@ -13,19 +13,19 @@ enum Result {
     Trap,
 }
 
-pub enum StoreInst {
-    Func(FuncInst),
+pub enum StoreInst<'a> {
+    Func(FuncInst<'a>),
     // Table(TableInst),
     // Mem(MemInst),
     // Global(GlobalInst),
 }
 
 #[derive(Default)]
-pub struct Store {
-    insts: Vec<StoreInst>,
+pub struct Store<'a> {
+    insts: Vec<StoreInst<'a>>,
 }
 
-impl Store {
+impl<'a> Store<'a> {
     fn funcs(&self) -> Vec<&FuncInst> {
         let mut res = vec![];
         for inst in self.insts.iter() {
@@ -55,16 +55,17 @@ pub struct ModuleInst {
 
 type HostFunc = String; // primitiveは関数名をStringで持つことにします
 
-enum FuncInst {
-    Normal { func_type: FuncType, code: Func }, // module instanceは関数で取得するようにします
+enum FuncInst<'a> {
+    Normal { func_type: FuncType, module: &'a ModuleInst, code: Func }, // module instanceは関数で取得するようにします
     Host { func_type: FuncType, host_code: HostFunc },
 }
 
-impl FuncInst {
-    fn module_instance() -> &ModuleInst {
+// impl FuncInst {
+//     fn module_instance() -> &ModuleInst {
         
-    }
-}
+//     }
+// }
+
 // type FuncElem = Option<FuncAddr>;
 // struct TableInst { elem: Vec<FuncElem>, max: Option<u32> }
 
@@ -96,29 +97,78 @@ struct Frame {
 
 struct Activation(u32, Frame);
 
-pub fn instantiate(store: Store, module: &Module, extern_vals: Vec<ExternVal>) -> ModuleInst {
-    allocate_module(store, module, extern_vals, vec![])
+struct Runtime<'a> {
+    store: Store<'a>,    
+    module_inst: ModuleInst,
 }
 
-pub fn allocate_module<'a>(mut store: Store, module: &Module, extern_vals: Vec<ExternVal>, vals: Vec<Val>) -> ModuleInst {
-    let mut module_inst = ModuleInst::default();
+impl<'a> Runtime<'a> {
+    fn new(module: &Module, store: Store<'a>) -> Runtime<'a> {
+        let mut module_inst = ModuleInst::default();
 
-    // set types
-    module_inst.types = module.types.clone();
+        module_inst.types = module.types.clone();
 
+        for func in module.funcs.iter() {
+            let address = allocate_func(&mut store, func, &module_inst);
+            module_inst.func_addrs.push(address);            
+        }
 
-    // let rc = Rc::new(&module_inst);
-    // let weak = Rc::downgrade(&rc);
-
-    // set funcinsts and funcaddrs
-    for func in module.funcs {        
-        module_inst.func_addrs.push(allocate_func(&mut store, func, module_inst));
+        Runtime {
+            store: store,            
+            module_inst: module_inst,
+        }
     }
 
-    module_inst
+    fn instantiate(&mut self, module: &Module) {
+        // let mut module_inst = ModuleInst::default();
+
+        
+
+        // self.module_inst = module_inst;
+    }
+
+    // fn allocate_func(&mut self, func: &Func) -> (FuncAddr, FuncInst) {
+    //     // 1. Let "func" be the <function> to allocate "moduleinst" its <module instance>.
+
+    //     // 2. Let "a" be the first free <function address> in S.
+    //     let address = self.store.insts.len() as FuncAddr;
+
+    //     // 3. Let "functype" be the <function type> "moduleinst".'types'["func".'type'].
+    //     let func_type = &self.module_inst.types[func.func_type.type_index()];
+
+    //     // 4. Let "funcinst" be the <function instance> {'type' "functype", 'module' "moduleinst" 'code' "func"}.
+    //     let func_inst = FuncInst::Normal { func_type: func_type.clone(), module: &self.module_inst, code: func.clone() };
+
+    //     (address, func_inst)
+    // }
 }
 
-fn allocate_func(store: &mut Store, func: Func, module_inst: ModuleInst) -> FuncAddr {
+pub fn instantiate(store: Store, module: &Module, extern_vals: Vec<ExternVal>) -> ModuleInst {
+    // allocate_module(store, module, extern_vals, vec![])    
+
+    let runtime = Runtime::new(module, store);
+    runtime.module_inst
+}
+
+// pub fn allocate_module<'a>(mut store: Store, module: &Module, extern_vals: Vec<ExternVal>, vals: Vec<Val>) -> ModuleInst {
+//     let mut module_inst = ModuleInst::default();
+
+//     // set types
+//     module_inst.types = module.types.clone();
+
+
+//     // let rc = Rc::new(&module_inst);
+//     // let weak = Rc::downgrade(&rc);
+
+//     // set funcinsts and funcaddrs
+//     for func in module.funcs {        
+//         module_inst.func_addrs.push(allocate_func(&mut store, func, module_inst));
+//     }
+
+//     module_inst
+// }
+
+fn allocate_func<'a>(store: &mut Store<'a>, func: &Func, module_inst: &'a ModuleInst) -> FuncAddr {
     // 1. Let "func" be the <function> to allocate "moduleinst" its <module instance>.
 
     // 2. Let "a" be the first free <function address> in S.
