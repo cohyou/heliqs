81935098c394bcb2dad5a563a129b2609ae10cb9
cohyou <cohyou@aurola.net>
2019-09-03 22:53:41 +0900

    making decoder...

--- src/decoder/mod.rs
+++ src/decoder/mod.rs
@@ -1,2 +1,26 @@
 // use parser::*;
 
+type Byte = u8;
+
+fn decode_to_integer(bytes: &[u8]) -> usize {
+    let mut value = 0usize;
+    for byte in bytes {
+        if *byte < 0x80 {
+            value += *byte as usize;
+            break;
+        } else {
+            use std::convert::TryInto;
+            let temp = *byte - 0x80;
+            let temp: usize = temp.try_into().unwrap();
+            value += temp;
+        }
+    }
+    value
+}
+
+#[test]
+fn test() {
+    assert_eq!(decode_to_integer(&[0]), 0);
+    assert_eq!(decode_to_integer(&[100]), 100);
+    assert_eq!(decode_to_integer(&[200]), 100);
+}
